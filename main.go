72783e4a353a979c69d686ab39485696924463d8
bnyatoro <nyatorobravian@gmail.com>
2024-10-20 13:32:14 +0300

    feat: implement function calls for web mirroring flags

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"log"
+
 	"wget/utils"
 )
 
 func main() {
-	output, url, background, file, rateLimit := utils.CheckFlags()
+	output, url, background, file, rateLimit, mirror, reject, exclude, convertLinks := utils.CheckFlags()
+
+	if mirror {
+		// Handle mirroring
+		if url == "" {
+			log.Fatal("URL is required for mirroring")
+		}
+		err := utils.MirrorWebsite(url, reject, exclude, convertLinks)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
 
 	// Handle multi-file download case
 	if file != "" {
@@ -14,7 +27,6 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		
 		// Pass rate limit to concurrent download function
 		err = utils.DownloadFilesConcurrently(urls, output, background, rateLimit)
 		if err != nil {
@@ -24,10 +36,12 @@ func main() {
 	}
 
 	// Handle single file download case
+	if url == "" {
+		log.Fatal("URL is required for single file download")
+	}
 	filename := output
 	if filename == "" {
 		filename = utils.GetFileName(url)
 	}
-
 	utils.DownloadWithLogging(url, filename, background, rateLimit)
 }
