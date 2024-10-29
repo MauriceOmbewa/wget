41168d7ecef9a4df5d4f2adf3335c62bfc52031e
ombima56 <ombimahillary6@gmail.com>
2024-10-29 12:06:21 +0300

    feat: Add test for DownloadFilesConcurrently function and error handling

--- utils/multiple_test.go
+++ utils/multiple_test.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"path/filepath"
+	"strconv"
 	"testing"
 )
 
@@ -33,6 +37,42 @@ func TestReadUrlsFromFile(t *testing.T) {
 	for i, url := range urls {
 		if url != expected[i] {
 			t.Errorf("expected URL %s, got %s", expected[i], url)
+		}
+	}
+}
+
+func TestDownloadFilesConcurrently(t *testing.T) {
+	// Set up a test server that serves a sample response for download
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("test content"))
+	}))
+	defer server.Close()
+
+	// Mock URLs pointing to the test server
+	urls := []string{server.URL, server.URL}
+
+	// Create a temporary directory to store downloaded files
+	outputDir, err := os.MkdirTemp("", "downloads")
+	if err != nil {
+		t.Fatalf("could not create temp dir: %v", err)
+	}
+	defer os.RemoveAll(outputDir)
+
+	// Test DownloadFilesConcurrently function
+	outputPrefix := "test_file"
+	rateLimit := int64(1024) // Set to 1KB/s for testing rate limiting
+
+	err = DownloadFilesConcurrently(urls, outputPrefix, false, rateLimit, outputDir)
+	if err != nil {
+		t.Fatalf("DownloadFilesConcurrently failed: %v", err)
+	}
+
+	// Verify that files were downloaded
+	for i := range urls {
+		filename := filepath.Join(outputDir, outputPrefix+"_"+strconv.Itoa(i))
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			t.Errorf("expected file %s to be downloaded, but it was not found", filename)
 		}
 	}
 }
