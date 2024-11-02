4e6ed16e59ca3db9b50917700a0310ec1ecb1cb2
bravian1 <nyatorobravian@gmail.com>
2024-11-02 10:52:00 +0300

    feat: ensure a file finishes downloading before the program exits

--- utils/download.go
+++ utils/download.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -13,19 +14,14 @@ func DownloadFile(urlStr, fileName string, background bool, rateLimit int64) err
     startTime := time.Now().Format("2006-01-02 15:04:05")
     fmt.Printf("start at %s\n", startTime)
 
-    // Create an HTTP client
     client := &http.Client{}
-
-    // Create a new request
     req, err := http.NewRequest("GET", urlStr, nil)
     if err != nil {
         return fmt.Errorf("error creating request: %v", err)
     }
 
-    // Set a User-Agent header to simulate a browser
     req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 
-    // Send the request
     resp, err := client.Do(req)
     if err != nil {
         return fmt.Errorf("error: %v", err)
@@ -48,7 +44,6 @@ func DownloadFile(urlStr, fileName string, background bool, rateLimit int64) err
 
     fmt.Printf("saving file to: ./%s\n", fileName)
 
-    // Create rate-limited reader if rate limit is specified else download normal
     var reader io.Reader = resp.Body
     if rateLimit > 0 {
         fmt.Printf("Rate limit set to: %.2f KB/s\n", float64(rateLimit)/1024)
@@ -76,37 +71,59 @@ func DownloadFile(urlStr, fileName string, background bool, rateLimit int64) err
     return nil
 }
 
+// Create a WaitGroup to track background downloads
+var downloadWg sync.WaitGroup
 
 func DownloadWithLogging(urlStr string, fileName string, background bool, rateLimit int64) {
-	if background {
-		fmt.Println("Output will be written to 'wget-log'.")
-		go func() {
-			logFile, err := os.Create("wget-log")
-			if err != nil {
-				fmt.Printf("Error: %v\n", err)
-				return
-			}
-			defer logFile.Close()
+    if background {
+        fmt.Println("Output will be written to 'wget-log'.")
+        
+        // Add to WaitGroup before starting goroutine
+        downloadWg.Add(1)
+        
+        go func() {
+            // Ensure WaitGroup is decremented when done
+            defer downloadWg.Done()
+            
+            logFile, err := os.Create("wget-log")
+            if err != nil {
+                fmt.Printf("Error: %v\n", err)
+                return
+            }
+            defer logFile.Close()
 
-			os.Stdout = logFile
-			os.Stderr = logFile
+            // Create new files for stdout and stderr
+            oldStdout := os.Stdout
+            oldStderr := os.Stderr
+            os.Stdout = logFile
+            os.Stderr = logFile
+            
+            // Restore original stdout/stderr when done
+            defer func() {
+                os.Stdout = oldStdout
+                os.Stderr = oldStderr
+            }()
 
-			err1 := DownloadFile(urlStr, fileName, background, rateLimit)
-			if err1 != nil {
-				fmt.Fprintln(logFile, "Error:", err)
-			}
-		}()
-		time.Sleep(3 * time.Second)
-	} else {
-		err := DownloadFile(urlStr, fileName, background, rateLimit)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
+            err1 := DownloadFile(urlStr, fileName, background, rateLimit)
+            if err1 != nil {
+                fmt.Fprintln(logFile, "Error:", err1)
+            }
+        }()
+
+        // Wait a moment for the download to start
+        time.Sleep(1 * time.Second)
+        
+        // Wait for background download to complete
+        downloadWg.Wait()
+    } else {
+        err := DownloadFile(urlStr, fileName, background, rateLimit)
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
 }
-func GetFileName(url string) string {
-	s := strings.Split(url, "/")
 
-	// Extract the file name from the URL path
-	return s[len(s)-1]
+func GetFileName(url string) string {
+    s := strings.Split(url, "/")
+    return s[len(s)-1]
 }
